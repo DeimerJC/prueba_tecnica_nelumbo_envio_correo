c6d471c9a932307f71c30d2c766a93fcfff29245
DeimerJC <joelcegarra21@gmail.com>
2023-11-05 12:54:48 -0500

    first commit

---
+++ src/main/java/com/ptn/prueba_tecnica_nelumbo_envio_correo/PruebaTecnicaNelumboEnvioCorreoApplication.java
@@ -0,0 +1,13 @@
+package com.ptn.prueba_tecnica_nelumbo_envio_correo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaTecnicaNelumboEnvioCorreoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaTecnicaNelumboEnvioCorreoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ptn/prueba_tecnica_nelumbo_envio_correo/PruebaTecnicaNelumboEnvioCorreoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ptn.prueba_tecnica_nelumbo_envio_correo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaTecnicaNelumboEnvioCorreoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
